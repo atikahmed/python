8ae0649e5592f4ef7e0a7fb023613ac550480191
Atik Ahmed <atikahmed@users.noreply.github.com>
2025-03-23 17:03:50 +0100

    Update remove_duplicet.py

--- src/remove_duplicet.py
+++ src/remove_duplicet.py
@@ -1,1 +1,10 @@
 
+def remove_duplicet(items):
+  unique = []
+  for item in items:
+      if item not in unique :
+        unique.append(item)
+  return unique 
+      
+number =[12,2,4,7,9,78,20,2,12]
+print (remove_duplicet(number))
