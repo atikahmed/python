c20e7d0fd3982c9b3b3795428939e3f18a4f8998
root <root@localhost>
2025-03-23 17:35:24 +0100

    auto publish 1742747724526

--- src/main.py
+++ src/main.py
@@ -1,3 +1,9 @@
-friends = [ "Batman", "Cat Man", "Fat Man", "Cockroach Man"] 
-cat_man_index = friends.index("Cat Man")
-print(cat_man_index)
+def count_vowels(sentence):
+  count = 0
+  vowels = ['a','e','i','o','u','A','E','I','O','U']
+  for char in sentence:
+     if char in vowels:
+        count += 1
+  return count
+
+print(count_vowels("Hello World"))
